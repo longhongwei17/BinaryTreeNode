d7394c5a0830e5899f78855542caff05ebe0ba3a
Apple <apple@appledemacbook-pro.local>
2017-03-05 10:51:41 +0800

    Initial Commit

---
+++ BinaryTreeNode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BinaryTreeNode
+//
+//  Created by Apple on 2017/3/5.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
